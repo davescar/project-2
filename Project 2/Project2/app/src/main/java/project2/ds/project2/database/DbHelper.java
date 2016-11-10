4313e446f8e11ae5c111997038927c61d3728692
dave-scar <david.scarimbolo@gmail.comgit config --global user.email david.scarimbolo@gmail.com>
2016-11-10 09:55:07 -0500

    2nd commmit

--- Project 2/Project2/app/src/main/java/project2/ds/project2/database/DbHelper.java
+++ Project 2/Project2/app/src/main/java/project2/ds/project2/database/DbHelper.java
@@ -19,7 +19,7 @@ import project2.ds.project2.R;
 
 public class DbHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 4;
+    private static final int DATABASE_VERSION = 5;
     public static final String DATABASE_NAME = "ANIMALS_DB";
     public static final String ANIMAL_LIST_TABLE_NAME = "ANIMAL_LIST";
 
@@ -195,13 +195,11 @@ public class DbHelper extends SQLiteOpenHelper {
 
     public void populateDatabase() {
 
-        //TODO: add more animals to the database just like below.
-
         Animal skunk = new Animal(149, 8, "Skunk", "Americas", "Beans, cabbage, garbage, eggs", "Temperamental, intelligent and smart. The more you hold and talk to her, the more love will come your way. Loves snuggling inside a T-shirt or tucked into a waistband. Hates the Yankees.", R.drawable.skunk, "Axe");
         addAnimal(skunk);
         Animal koala = new Animal(3200, 13, "Koala", "Australia", "Eucalyptus", "Somewhat housebroken. Loves Vin Diesel movies. Can be aggressive when coming down from Eucalyptus high. ", R.drawable.koala_eating, "GumNut");
         addAnimal(koala);
-        Animal bunny = new Animal(129000, 65, "Human", "USA", "Froyo, lettuce, Acai Berries", "Beautiful, friendly, and from a small town, Likes to bounce around, and hop on things. Easily distracted by shiny objects.", R.drawable.bunny, "Rosemary");
+        Animal bunny = new Animal(129000, 65, "Human", "USA", "Froyo, lettuce, Acai Berries", "Beautiful, friendly, and from a small town, Likes to bounce around, and hop on things. Easily distracted by shiny objects.", R.drawable.bunny, "Charlene");
         addAnimal(bunny);
         Animal unicorn = new Animal(3500, 15, "Unicorn", "Yugoslavia", "Elves, fairies, pixies", "A unicorn's horn is said to have the power to render poisoned water potable and to heal sickness.", R.drawable.unicorn, "Headstick");
         addAnimal(unicorn);
@@ -209,11 +207,14 @@ public class DbHelper extends SQLiteOpenHelper {
         addAnimal(goat);
         Animal blobFish = new Animal(12, 130, "Fish", "New Zealand", "Anything fat-free", "Official Mascot of the UAPS (Ugly Animal Preservation Society) Excellent deep-sea diver. Big-boned.", R.drawable.blobfish, "Anjellica");
         addAnimal(blobFish);
-        Animal rock = new Animal(1500000, 130, "Fish", "New Zealand", "Anything fat-free", "Official Mascot of the UAPS (Ugly Animal Preservation Society) Excellent deep-sea diver. Big-boned.", R.drawable.petrock, "Anjellica");
+        Animal rock = new Animal(3, 15000000, "Sedimentary", "New York", "Vegan", "Social, hard-headed, likes company. This rock will never let you down, and offers great protection against giants.", R.drawable.petrock, "Norm");
         addAnimal(rock);
-
-//       TODO: ADD  MORE ANIMALS!!!!
-
+        Animal AyeAye = new Animal(900, 21, "Aye-Aye", "Madagascar", "Grubs, fruit, sap, chicken wings", "Finger-model. Third-eye (lid). Once thought to be extinct, Aye-Ayes are here to stay.", R.drawable.ayeaye, "Captain");
+        addAnimal(AyeAye);
+        Animal Tardigrade = new Animal(45, 1, "Tardigrade", "Space", "Bacteria Eater", "Smarter than your average bear. Won't fuss with the thermostat. This little guy survived a visit to space. Loves radiation.", R.drawable.tardigrae, "Indo");
+        addAnimal(Tardigrade);
+        Animal Naked_Mole_Rat = new Animal(6000, 31, "Naked Mole Rat", "East Africa", "Tubers", " Neither moles nor rats, Naked Mole Rats are not totally hairless. Say what! The Mole Rat species in one of the two mammals that are eusocial", R.drawable.nakedmolerat, "Teef");
+        addAnimal(Naked_Mole_Rat);
 
     }
 }
